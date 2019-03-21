486eecd9b7b0f4c765fec3f962eca4fb0bbadd55
Lucio Henrique <luciobsg123@hotmail.com>
2019-03-21 19:38:41 -0300

    adicionado o back-end

---
+++ server/Tifs/Models/Cabeleireiro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tifs.Models
+{
+    public class Cabeleireiro
+    {
+        [Key]
+        public int IdCabeleireiro { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ server/Tifs/Models/TifsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Tifs.Models
+{
+    public class TifsContext : DbContext
+    {
+        public TifsContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Tifs.Models.Cliente> Clientes { get; set; }
+    }
+}
