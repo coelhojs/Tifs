fa6a298957422af0247223c7fda34385925bdfec
coelhojs <ccoelho.me@gmail.com>
2019-03-22 00:41:03 -0300

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import CadastroCliente from "./components/CadastroCliente";
+import Home from "./components/Home";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Header />
-                <main className="container-fluid">
-                    <CadastroCliente />
-                </main>
-            </div>
+            <Router>
+                <div>
+                    <Header />
+
+                    <Route exact path="/" component={Home} />
+                    {/* <Route path="/about" component={About} />
+                    <Route path="/topics" component={Topics} /> */}
+                </div>
+            </Router>
         );
     }
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="text-center">
+                <img src="/img/logo-tifs.png" alt="" />
+            </div>
+        );
+    }
+}
+
+export default Home;
