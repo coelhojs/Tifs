64f9bddba2672d2680475401db2dd01d96c23134
lbltavares <lucas.laborne@gmail.com>
2019-05-16 18:01:49 -0300

    Mudancas no layout

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+const mainContentStyle = {
+    'padding': 30,
+    'background-color': 'rgba(100, 100, 100, 0.1)',
+}
+
 class Home extends Component {
     render() {
         return (
-            <main className="container-fluid">
+            <main style={mainContentStyle}>
                 <div className="row d-flex justify-content-around">
-                    <Link to="/Servico" className="btn m-2 btn-dark col-5">Novo Servico</Link>
-                    <Link to="/Clientes" className="btn m-2 btn-dark col-5">Meus Clientes</Link>
-                    <Link to="/Historico" className="btn m-2 btn-dark col-5">Histórico de serviços</Link>
-                    <Link to="/Relatorios" className="btn m-2 btn-dark col-5">Relatórios</Link>
-                    <Link to="/Produtos" className="btn m-2 btn-dark col-5">Produtos</Link>
+                    <Link to="/Servico" className="btn-lg m-2 btn-dark col-3">
+                    <img src="./img/icones/essential/svg/add-1.svg" style={{ width: 40, marginRight: 30 }} />
+                    Novo Servico</Link>
+                </div>
+                <div className="row d-flex justify-content-around">
+                    <Link to="/Clientes" className="btn-lg m-2 btn-dark col-3">
+                    <img src="./img/icones/essential/svg/users-1.svg" style={{ width: 40, marginRight: 30 }} />
+                        Meus Clientes
+                    </Link>
+                </div>
+                <div className="row d-flex justify-content-around">
+                    <Link to="/Historico" className="btn-lg m-2 btn-dark col-3">
+                    <img src="./img/icones/essential/svg/hourglass-1.svg" style={{ width: 40, marginRight: 30 }} />
+                    Histórico de serviços</Link>
+                </div>
+                <div className="row d-flex justify-content-around">
+                    <Link to="/Relatorios" className="btn-lg m-2 btn-dark col-3">
+                    <img src="./img/icones/essential/svg/notebook-1.svg" style={{ width: 40, marginRight: 30 }} />
+                    Relatórios</Link>
+                </div>
+                <div className="row d-flex justify-content-around">
+                    <Link to="/Produtos" className="btn-lg m-2 btn-dark col-3">
+                    <img src="./img/icones/essential/svg/produtos.svg" style={{ width: 40, marginRight: 30 }} />
+                    Produtos</Link>
+                </div>
+                <div className="text-center" style={{ marginTop: 80 }}>
+                    <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" className="fa fa-facebook-square fa-3x social"></i></a>
+                    <a href="https://twitter.com/bootsnipp"><i id="social-tw" className="fa fa-twitter-square fa-3x social"></i></a>
+                    <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" className="fa fa-google-plus-square fa-3x social"></i></a>
+                    <a href="mailto:bootsnipp@gmail.com"><i id="social-em" className="fa fa-envelope-square fa-3x social"></i></a>
                 </div>
             </main>
         );
     }
 }
+
 
 export default Home;
--- client/src/containers/UserMenu.js
+++ client/src/containers/UserMenu.js
@@ -4,23 +4,39 @@ import { pushRotate as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import { getCabeleireiroName } from '../actions/cabeleireiro';
 
+
+
 class UserMenu extends Component {
-    // componentDidMount() {
-    //     let id = "5cdca4592285902f5c36802a";
-    //     this.props.getCabeleireiroName(id);
-    // }
+    componentDidMount() {
+        let id = "5cdca4592285902f5c36802a";
+        this.props.getCabeleireiroName(id);
+    }
 
     render() {
         return (
             <Menu pageWrapId={"page-wrap"} >
-                {/* <div>
-                    <img src="/img/icon/female.png" alt="" />
-                    <span>Olá, {this.props.cabeleireiro}</span>
-                </div> */}
-                <Link to="/Editar/5cdca4592285902f5c36802a" className="menu-item">Editar</Link>
-                <Link to="/Ajuda" className="menu-item">Ajuda</Link>
-                <Link to="/Termos" className="menu-item">Termos</Link>
-                <Link to="/" className="menu-item">Sair</Link>
+                <div style={{ marginBottom: 60 }}>
+                    <img src="/img/icones/essential/svg/user-3.svg" width="70px" />
+                </div>
+                <span>Menu Principal</span>
+                <hr />
+                <Link to="/Editar/5cdca4592285902f5c36802a" className="menu-item" style={{ marginBottom: 20 }}>
+                    <img src="/img/icones/essential/svg/edit.svg" width="20px" />
+                    <span style={{ marginLeft: 30 }}>Editar</span>
+                </Link>
+                <Link to="/Ajuda" className="menu-item" style={{ marginBottom: 20 }}>
+                    <img src="/img/icones/essential/svg/help.svg" width="20px" />
+                    <span style={{ marginLeft: 30 }}>Ajuda</span>
+                </Link>
+                <Link to="/Termos" className="menu-item" style={{ marginBottom: 20 }}>
+                    <img src="/img/icones/essential/svg/notepad-2.svg" width="20px" />
+                    <span style={{ marginLeft: 30 }}>Termos</span>
+                </Link>
+                <hr />
+                <Link to="/" className="menu-item" style={{ marginBottom: 20 }}>
+                    <img src="/img/icones/essential/svg/exit-2.svg" width="20px" />
+                    <span style={{ marginLeft: 30 }}>Sair</span>
+                </Link>
             </Menu>
         )
     }
